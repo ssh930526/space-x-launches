20df6a4111dbcc36b3cabe5e08a7ade4e573ef45
Sihui Shen <sihui@Sihuis-MBP.fios-router.home>
2021-03-16 23:51:49 -0400

    feat: finishes mvp

--- js/script.js
+++ js/script.js
@@ -1,19 +1,49 @@
-//constant variables -data that never changes
-alert('Js is loaded!');
-//state variables- data that changes
-
-// cached element refrence - parts of the dom we need to touch
-
-//event listeners - capture and respond to events i.e. use clicks on something
-
-// functions- code that represents actions taken/carried out
-
-function getData(){ 
-
-     $.ajax('https://api.spacexdata.com/v3/launches')
-         .then(function(data) { 
-             console.log(data); 
-         }, function(error) {
-            console.log(error);
-         });
+// constant variables - data that never changes
+const BASE_URL = 'https://api.spacexdata.com/v3/launches';
+// state variables - data that changes
+let launches;
+// cached element references - parts of the dom we need to touch
+const $launches = $('#launches');
+// event listeners - capture and respond to events i.e. user clicks on something
+// functions - code that represents actions taken/carried out
+init();
+function init() {
+    getData();
 }
+function getData() {
+    $.ajax(BASE_URL + "?limit=12")
+        .then(function (data) {
+            launches = data;
+            render();
+        }, function (error) {
+            console.log(error);
+        });
+}
+function render() {
+    const html = launches.map(function(launch) {
+        return `
+            <article class="card">
+                <h1>${launch.mission_name}</h1>
+                <p>${launch.launch_year}</p>
+            </article>
+        `;
+    });
+    $launches.append(html);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
